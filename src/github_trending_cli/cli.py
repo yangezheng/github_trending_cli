b60331afdd0f400c2ea3c07c1d4718376ba9ebe4
Yange <posterscofield@gmail.com>
2026-02-11 20:22:45 +0100

    argparse works

--- src/github_trending_cli/cli.py
+++ src/github_trending_cli/cli.py
@@ -1,2 +1,28 @@
+import argparse
+from .core import parse_duration,since_date, build_query
+from .github_api import search_query_from
+from .errors import InvalidDurationError, InvalidLimitError, GitHubAPIError
+
 def main():
-    raise NotImplementedError
+    parser = argparse.ArgumentParser(description="GitHub Trending CLI")
+    parser.add_argument('-d', "--duration", default='1w', help='1w, 1m, 3m, 6m, 1y')
+    parser.add_argument('-l',"--limit", type = int, default=10)
+    args = parser.parse_args()
+
+    try:
+        days = parse_duration(args.duration)
+        since = since_date(days)
+        query = build_query(since)
+
+        repos = search_query_from(query=query, limit = args.limit)
+
+        for repo in repos:
+            print(f"{repo['full_name']} with {repo['stargazers_count']} stars")
+        
+    except InvalidDurationError as e:
+        print(f"Error: {e}")
+    except GitHubAPIError as e:
+        print(f"API Error: {e}")
+    
+if __name__ == "__main__":
+    main()
