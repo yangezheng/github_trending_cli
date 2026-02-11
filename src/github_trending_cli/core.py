70e3ff7896bee668d972219ca2fac8e32d90db93
Yange <posterscofield@gmail.com>
2026-02-11 19:54:30 +0100

    finished core

--- src/github_trending_cli/core.py
+++ src/github_trending_cli/core.py
@@ -1,15 +1,29 @@
+from .errors import InvalidDurationError
+from datetime import date, timedelta
+_ALLOWED = {
+    '1w': 7,
+    '1m': 30,
+    '3m': 90,
+    '6m': 180,
+    '1y': 365,
+}
 
 # parse duration to number of days
 # e.g. 1 week -> 7
-def _parse_duration(duration_str) -> int:
-    pass
+def parse_duration(duration_str) -> int:
+    if duration_str not in _ALLOWED:
+        raise InvalidDurationError(f"duration must be one of".join(_ALLOWED))
+    return _ALLOWED[duration_str]
 
 
 # build since YYYY-MM-DD
 # from days 
-def _sinse_date(days) -> str:
-    pass
+def since_date(days: int, today: date | None = None) -> str:
+    today = today or date.today()
+    since_date = today-timedelta(days = days)
+    return since_date.isoformat()
+
 
 # build query
-def _build_query(since_date) -> str:
-    pass
+def build_query(since_date: str) -> str:
+    return f"created:>={since_date}"
