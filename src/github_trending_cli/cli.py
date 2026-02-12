bbcca1fcad706b4845363c0183d672c23e17d866
Yange <posterscofield@gmail.com>
2026-02-12 10:07:38 +0100

    updated cli helper

--- src/github_trending_cli/cli.py
+++ src/github_trending_cli/cli.py
@@ -4,9 +4,9 @@ from .github_api import search_query_from
 from .errors import InvalidDurationError, InvalidLimitError, GitHubAPIError
 
 def main():
-    parser = argparse.ArgumentParser(description="GitHub Trending CLI")
-    parser.add_argument('-d', "--duration", default='1w', help='1w, 1m, 3m, 6m, 1y')
-    parser.add_argument('-l',"--limit", type = int, default=10)
+    parser = argparse.ArgumentParser(description="GitHub Trending CLI, use this library to retrieve the most stared repos for the last period of time")
+    parser.add_argument("-d" , "--duration", default='1w', help="To get the last period of repos, valid args: [1w, 1m, 3m, 6m, 1y], default is '1w'")
+    parser.add_argument('-l',"--limit", type = int, default=10, help="set the number of repos you want to retrieve")
     args = parser.parse_args()
 
     try:
